2918cea5ebe1539076f60b06cddd309aae1386c0
Omar Vásquez <omarvs91@gmail.com>
2024-07-13 14:47:22 -0500

    Adding custom model filter so GC filters the permisos if user only has jefatura rol.

---
+++ app/Models/PermisosGerenciaCustom.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use GroceryCrud\Core\Model;
+
+class PermisosGerenciaCustom extends Model
+{
+    public function extraWhereStatements($select)
+    {
+        // Join the 'usuarios' table with 'areas' on 'id_area' to access 'id_jefe'
+        $select->join('usuarios', 'usuarios.id = registro_permisos.id_usuario')
+            ->join('areas', 'areas.id = usuarios.id_area')
+            ->where("areas.id_jefe = '" . session()->get('user_id') . "'"); // Filter where 'id_jefe' equals 38
+
+        return $select;
+    }
+}
