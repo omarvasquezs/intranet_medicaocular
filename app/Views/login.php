e50bada03ffa39d8ab6b33db99e967f525faa4ba
Omar Vásquez <omarvs91@gmail.com>
2024-07-27 23:50:15 -0500

    Adding error session alert on login page.

--- app/Views/login.php
+++ app/Views/login.php
@@ -18,24 +18,32 @@
 
 <body class="login-page">
     <div class="container">
-        <div class="login-container">
-            <img class="img-fluid mb-4" src="https://medicaocular.pe/wp-content/uploads/2023/08/Logotipo-Medica-Ocular.png">
+        <?php if (session()->has('error')): ?>
+            <div class="alert alert-danger alert-dismissible fade show mt-2" role="alert">
+                <?= session('error') ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php endif; ?>
+        <div class="login-container rounded-0">
+            <img class="img-fluid mb-4"
+                src="https://medicaocular.pe/wp-content/uploads/2023/08/Logotipo-Medica-Ocular.png">
             <form class="login-form" action="/authenticate" method="post">
                 <div class="mb-3">
-                    <input type="text" class="form-control" id="username" name="username" placeholder="USUARIO"
-                        required>
+                    <input type="text" class="form-control rounded-0" id="username" name="username"
+                        placeholder="USUARIO" required>
                 </div>
-                <div class="mb-4">
-                    <input type="password" class="form-control" id="password" name="password" placeholder="CONTRASEÑA"
-                        required>
+                <div class="mb-5">
+                    <input type="password" class="form-control rounded-0" id="password" name="password"
+                        placeholder="CONTRASEÑA" required>
                 </div>
                 <div class="d-grid gap-2">
-                    <button type="submit" class="btn btn-primary login-button">ACCEDER</button>
+                    <button type="submit" class="btn btn-primary login-button rounded-0 border-info">ACCEDER</button>
                 </div>
             </form>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     <!-- CUSTOM JS -->
     <script type="text/javascript" src="<?= base_url() ?>themes/medicaocular/theme.js"></script>
