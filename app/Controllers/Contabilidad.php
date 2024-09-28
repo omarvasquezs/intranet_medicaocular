c99ba2d2d0af3012bd253b7079785929f25e08eb
Omar Vásquez <omarvs91@gmail.com>
2024-09-27 22:29:17 -0500

    Removing contabilidad_boletas_rechazadas from contabilidad controller since all uploaded boletas are approved internally by gerencia.

--- app/Controllers/Contabilidad.php
+++ app/Controllers/Contabilidad.php
@@ -124,43 +124,4 @@ class Contabilidad extends BaseController
         $output = $this->gc->render();
         return $this->mainOutput($output);
     }
-    public function contabilidad_boletas_rechazadas()
-    {
-        $this->gc->setTable('boletas')
-            ->setSubject('BOLETA', 'BOLETAS RECHAZADAS')
-            ->defaultOrdering('boletas.fecha_creacion', 'desc')
-            ->unsetEdit()
-            ->unsetDelete()
-            ->unsetExport()
-            ->unsetPrint()
-            ->unsetFilters()
-            ->unsetAdd()
-            ->where(['id_estado_boleta' => 3])
-            ->setRead()
-            ->setRelation('id_estado_boleta', 'estado_boletas', 'estado_boleta')
-            ->setRelation('id_usuario', 'usuarios', 'nombres')
-            ->setRelation('subido_por', 'usuarios', 'nombres')
-            ->setRelation('revisado_por', 'usuarios', 'nombres')
-            ->setFieldUpload('adjunto', 'assets/uploads/boletas/', base_url() . 'assets/uploads/boletas/', [
-                'maxUploadSize' => '20M', // 20 Mega Bytes
-                'minUploadSize' => '1K', // 1 Kilo Byte
-                'allowedFileTypes' => ['pdf']
-            ])
-            ->unsetColumns(['subido_por', 'id_estado_boleta', 'fecha_modificacion', 'observaciones'])
-            ->unsetSearchColumns(['adjunto'])
-            ->displayAs([
-                'id_usuario' => 'EMPLEADO',
-                'fecha_creacion' => 'FECHA DE CREACION',
-                'adjunto' => 'BOLETA',
-                'id_estado_boleta' => 'ESTADO DE LA BOLETA',
-                'subido_por' => 'SUBIDO POR',
-                'fecha_modificacion' => 'FECHA DE MODIFICACION',
-                'revisado_por' => 'REVISADO POR',
-                'observaciones' => 'OBSERVACIONES'
-            ]);
-
-        // Rendering the CRUD
-        $output = $this->gc->render();
-        return $this->mainOutput($output);
-    }
 }
