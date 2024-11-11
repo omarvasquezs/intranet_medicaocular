3be4476eb9de55fbd076f68b75ce4de1df6daedf
Omar Vásquez <omarvs91@gmail.com>
2024-11-10 20:32:00 -0500

    Removing ckeditor5 css and js from output since it's not a very good practice.

--- app/Views/output.php
+++ app/Views/output.php
@@ -25,9 +25,6 @@
     <link href="<?= base_url() ?>themes/startbootstrap-sb-admin-2/css/sb-admin-2.min.css" rel="stylesheet">
     <link href="<?= base_url() ?>themes/medicaocular/style.css" rel="stylesheet">
     <!-- end -->
-    <link rel="stylesheet" href="https://cdn.ckeditor.com/ckeditor5/43.3.1/ckeditor5.css" />
-    <link rel="stylesheet"
-        href="https://cdn.ckeditor.com/ckeditor5-premium-features/43.3.1/ckeditor5-premium-features.css">
 </head>
 
 <body id="page-top">
@@ -411,110 +408,6 @@
     ?>
     <!-- end -->
     <!-- Custom theme JS -->
-    <!-- Load CKEditor from jsDelivr -->
-    <script type="importmap">
-    {
-        "imports": {
-            "ckeditor5": "https://cdn.ckeditor.com/ckeditor5/43.3.1/ckeditor5.js",
-            "ckeditor5/": "https://cdn.ckeditor.com/ckeditor5/43.3.1/",
-            "ckeditor5-premium-features": "https://cdn.ckeditor.com/ckeditor5-premium-features/43.3.1/ckeditor5-premium-features.js",
-            "ckeditor5-premium-features/": "https://cdn.ckeditor.com/ckeditor5-premium-features/43.3.1/"
-        }
-    }
-    </script>
-
-    <script type="module">
-        import {
-            ClassicEditor,
-            Essentials,
-            Bold,
-            Italic,
-            Font,
-            Paragraph,
-            SourceEditing,
-            CKFinder,
-            CKFinderUploadAdapter,
-            Link,
-            Image, ImageInsert, ImageCaption, AutoImage, ImageResize, ImageStyle, ImageResizeEditing, ImageResizeHandles,
-            ImageToolbar,
-            ImageUpload, PictureEditing, CloudServices,
-            Table, TableToolbar, LinkImage, ImageUploadEditing, ImageUploadProgress,
-            DecoupledEditor, Heading, HeadingButtonsUI, ParagraphButtonUI,
-            Indent, IndentBlock, BlockQuote, List, AutoLink, MediaEmbed, TodoList
-        } from 'ckeditor5';
-        import coreTranslations from 'ckeditor5/translations/es.js';
-        import premiumFeaturesTranslations from 'ckeditor5-premium-features/translations/es.js';
-
-        const editorElement = document.querySelector('#editor');
-        if (editorElement) {
-            ClassicEditor
-                .create(document.querySelector('#editor'), {
-                    language: 'es', // Set language to Spanish
-                    plugins: [
-                        SourceEditing, Essentials, Table, TableToolbar, Bold, Italic, Font, Paragraph,
-                        CKFinderUploadAdapter, Link, Image, AutoImage, PictureEditing,
-                        ImageToolbar, ImageInsert, ImageCaption, ImageStyle, ImageResize, ImageResizeEditing, ImageResizeHandles,
-                        LinkImage, ImageUpload, ImageUploadEditing, ImageUploadProgress, CloudServices, Heading, Indent, IndentBlock,
-                        BlockQuote, List, AutoLink, MediaEmbed, TodoList
-                    ],
-                    toolbar: [
-                        'undo', 'redo', '|',
-                        'heading',
-                        'bold', 'italic', '|',
-                        'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', '|',
-                        'insertImage', '|',
-                        'insertTable',
-                        'link',
-                        'mediaEmbed',
-                        'blockQuote',
-                        'bulletedList', 'numberedList', 'todoList',
-                        'outdent', 'indent',
-                        'SourceEditing'
-                    ],
-                    translations: [
-                        coreTranslations,
-                        premiumFeaturesTranslations
-                    ],
-                    ckfinder: {
-                        uploadUrl: '<?= base_url("upload") ?>',
-                        options: {
-                            resourceType: 'Images'
-                        }
-                    },
-                    image: {
-                        toolbar: [
-                            'toggleImageCaption',
-                            'imageTextAlternative',
-                            '|',  // Separator
-                            'imageStyle:Inline',
-                            'imageStyle:alignBlockLeft',
-                            'imageStyle:alignCenter',
-                            'imageStyle:alignBlockRight',
-                            '|',  // Separator
-                            'imageStyle:alignLeft',
-                            'imageStyle:alignRight',
-                        ]
-                    },
-                    heading: {
-                        options: [
-                            { model: 'paragraph', title: 'Párrafo', class: 'ck-heading_paragraph' },
-                            { model: 'heading1', view: 'h1', title: 'Título 1', class: 'ck-heading_heading1' },
-                            { model: 'heading2', view: 'h2', title: 'Título 2', class: 'ck-heading_heading2' },
-                            { model: 'heading3', view: 'h3', title: 'Título 3', class: 'ck-heading_heading3' },
-                            { model: 'heading4', view: 'h4', title: 'Título 4', class: 'ck-heading_heading4' },
-                            { model: 'heading5', view: 'h5', title: 'Título 5', class: 'ck-heading_heading5' },
-                            { model: 'heading6', view: 'h6', title: 'Título 6', class: 'ck-heading_heading6' }
-                        ]
-                    }
-                })
-                .then(editor => { console.log(editor); })
-                .catch(error => { console.error(error); });
-        } else {
-            console.log("No #editor element found.");
-        }
-
-    </script>
-
     <script type="text/javascript" src="<?= base_url() ?>themes/medicaocular/theme.js"></script>
     <!-- end -->
 </body>
