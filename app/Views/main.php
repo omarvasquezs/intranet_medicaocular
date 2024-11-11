cd1dc6734956190da7dd24eabe13f44150375811
Omar Vásquez <omarvs91@gmail.com>
2024-11-10 20:35:18 -0500

    Modifying main view to adapt it to the ckeditor5.

--- app/Views/main.php
+++ app/Views/main.php
@@ -1,54 +1,39 @@
-<div class="row mt-5">
+<div class="row">
     <div class="col-lg-9 col-md-12 mx-auto">
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="publicaciones-tab" data-toggle="tab" data-target="#publicaciones"
-                    type="button" role="tab" aria-controls="publicaciones" aria-selected="true">Publicaciones</button>
-            </li>
-            <?php if (array_intersect(session()->get('roles'), [1, 2])): ?>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="editar-tab" data-toggle="tab" data-target="#editar" type="button"
-                        role="tab" aria-controls="editar" aria-selected="false">Editar</button>
-                </li>
-            <?php endif; ?>
-        </ul>
-        <div class="tab-content p-2" id="myTabContent">
-            <div class="tab-pane fade show active" id="publicaciones" role="tabpanel"
-                aria-labelledby="publicaciones-tab">
+        <?php if (array_intersect(session()->get('roles'), [1, 2])): ?>
+            <div>
+                <a href="<?= base_url('editar_publicaciones') ?>" class="btn btn-primary rounded-0">
+                    <i class="fas fa-pencil-alt"></i> Editar Publicaciones
+                </a>
+            </div>
+        <?php endif; ?>
 
-                <?php if (!empty($publicaciones)): ?>
-                    <?php foreach ($publicaciones as $publicacion): ?>
-                        <?php if (!is_null($publicacion)): ?>
-                            <div class="row my-4">
-                                <div class="col-md-12">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <p class="card-text"><?php echo $publicacion['publicacion']; ?></p>
-                                            <div class="card-footer">
-                                                <div><small class="text-muted">Publicado:
-                                                        <?php echo $publicacion['fecha_creacion']; ?></small></div>
-                                                <div><small class="text-muted">Autor:
-                                                        <?php echo $publicacion['nombres']; ?></small></div>
-                                            </div>
-                                        </div>
+        <?php if (!empty($publicaciones)): ?>
+            <?php foreach ($publicaciones as $publicacion): ?>
+                <?php if (!is_null($publicacion)): ?>
+                    <div class="row my-4">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <p class="card-text"><?php echo $publicacion['publicacion']; ?></p>
+                                    <div class="card-footer">
+                                        <div><small class="text-muted">Publicado:
+                                                <?php echo $publicacion['fecha_creacion']; ?></small></div>
+                                        <div><small class="text-muted">Autor:
+                                                <?php echo $publicacion['nombres']; ?></small></div>
                                     </div>
                                 </div>
                             </div>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                <?php else: ?>
-                    <div class="row my-4">
-                        <div class="col-md-12">
-                            <h4>No hay publicaciones disponibles.</h4>
                         </div>
                     </div>
                 <?php endif; ?>
-
+            <?php endforeach; ?>
+        <?php else: ?>
+            <div class="row my-4">
+                <div class="col-md-12">
+                    <h4>No hay publicaciones disponibles.</h4>
+                </div>
             </div>
-            <div class="tab-pane fade" id="editar" role="tabpanel" aria-labelledby="editar-tab">
-                <iframe src="<?= base_url() ?>editar_publicaciones" frameborder="0" width="100%"
-                    style="height: 100%;min-height: 500px;"></iframe>
-            </div>
-        </div>
+        <?php endif; ?>
     </div>
 </div>
