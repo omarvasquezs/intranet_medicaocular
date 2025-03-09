540ccf943a0e5461989d1a0e2f2140242f9035aa
Omar Vásquez <omarvs91@gmail.com>
2025-03-09 00:36:22 -0500

    Quitando la opción de exportar pdf en el controlador Contabilidad y CTS y ajustando la lógica de inserción para manejar correctamente los campos 'boleta_firmada' y 'visto' en las operaciones de exportación.

--- app/Controllers/Contabilidad.php
+++ app/Controllers/Contabilidad.php
@@ -11,8 +11,8 @@ class Contabilidad extends BaseController
             ->setSubject('BOLETA', 'HISTORICO BOLETAS')
             ->defaultOrdering('boletas.fecha_creacion', 'desc')
             ->unsetEdit()
-            ->unsetExport()
             ->unsetPrint()
+            ->unsetExportPdf()
             ->unsetFilters()
             ->setRead()
             ->setRelation('id_estado_boleta', 'estado_boletas', 'estado_boleta')
@@ -36,21 +36,30 @@ class Contabilidad extends BaseController
             ->fieldType('visto', 'dropdown_search', [
                 0 => 'NO',
                 1 => 'SI'
-            ])
-            ->callbackBeforeInsert(function ($stateParameters) {
-                $filename = $stateParameters->data['adjunto'];
-                $fileExtension = pathinfo($filename, PATHINFO_EXTENSION);
-                if (strtolower($fileExtension) !== 'pdf') {
-                    $errorMessage = new \GroceryCrud\Core\Error\ErrorMessage();
-                    return $errorMessage->setMessage("Solo puede subir PDFs!\n");
-                }
-                $stateParameters->data['subido_por'] = session()->get('user_id');
-                $stateParameters->data['id_estado_boleta'] = 2;
-                $stateParameters->data['revisado_por'] = 5;
-                $stateParameters->data['boleta_firmada'] = 1; // Not signed yet
-                $stateParameters->data['visto'] = 0; // Not viewed yet
-                return $stateParameters;
-            })
+            ]);
+        // Check if current operation is export  
+        if ($this->gc->getState() === 'Export') {
+            $this->gc->callbackColumn('boleta_firmada', function ($value, $row) {
+                return ($value == '0') ? 'SI' : 'NO';
+            });
+            $this->gc->callbackColumn('visto', function ($value, $row) {
+                return ($value == '0') ? 'NO' : 'SI';
+            });
+        }
+        $this->gc->callbackBeforeInsert(function ($stateParameters) {
+            $filename = $stateParameters->data['adjunto'];
+            $fileExtension = pathinfo($filename, PATHINFO_EXTENSION);
+            if (strtolower($fileExtension) !== 'pdf') {
+                $errorMessage = new \GroceryCrud\Core\Error\ErrorMessage();
+                return $errorMessage->setMessage("Solo puede subir PDFs!\n");
+            }
+            $stateParameters->data['subido_por'] = session()->get('user_id');
+            $stateParameters->data['id_estado_boleta'] = 2;
+            $stateParameters->data['revisado_por'] = 5;
+            $stateParameters->data['boleta_firmada'] = 1; // Not signed yet
+            $stateParameters->data['visto'] = 0; // Not viewed yet
+            return $stateParameters;
+        })
             ->callbackAfterInsert(function ($stateParameters) {
                 $existingPdfPath = FCPATH . 'assets/uploads/boletas/' . $this->boletas->find($stateParameters->insertId)['adjunto'];
 
@@ -131,7 +140,7 @@ class Contabilidad extends BaseController
             ->setSubject('CTS', 'HISTORICO CTS')
             ->defaultOrdering('boletas_cts.fecha_creacion', 'desc')
             ->unsetEdit()
-            ->unsetExport()
+            ->unsetExportPdf()  // Changed from unsetExport() to unsetExportPdf()
             ->unsetPrint()
             ->unsetFilters()
             ->setRead()
@@ -147,7 +156,7 @@ class Contabilidad extends BaseController
             ->addFields(['id_usuario', 'adjunto'])
             ->unsetColumns(['subido_por', 'fecha_modificacion', 'observaciones'])
             ->unsetSearchColumns(['adjunto'])
-            ->columns(['id_usuario', 'adjunto', 'id_estado_boleta', 'fecha_creacion', 'revisado_por', 'boleta_firmada', 'visto'])
+            ->columns(['id_usuario', 'adjunto', 'fecha_creacion', 'boleta_firmada', 'visto'])
             ->readFields(['id_usuario', 'adjunto', 'id_estado_boleta', 'fecha_creacion', 'fecha_modificacion', 'subido_por', 'revisado_por', 'observaciones', 'boleta_firmada', 'visto'])
             ->fieldType('boleta_firmada', 'dropdown_search', [
                 1 => 'NO',
@@ -156,83 +165,92 @@ class Contabilidad extends BaseController
             ->fieldType('visto', 'dropdown_search', [
                 0 => 'NO',
                 1 => 'SI'
-            ])
-            ->callbackBeforeInsert(function ($stateParameters) {
-                $filename = $stateParameters->data['adjunto'];
-                $fileExtension = pathinfo($filename, PATHINFO_EXTENSION);
-                if (strtolower($fileExtension) !== 'pdf') {
-                    $errorMessage = new \GroceryCrud\Core\Error\ErrorMessage();
-                    return $errorMessage->setMessage("Solo puede subir PDFs!\n");
-                }
-                $stateParameters->data['subido_por'] = session()->get('user_id');
-                $stateParameters->data['id_estado_boleta'] = 2;
-                $stateParameters->data['revisado_por'] = 5;
-                $stateParameters->data['boleta_firmada'] = 1; // Not signed yet
-                $stateParameters->data['visto'] = 0; // Not viewed yet
-                return $stateParameters;
-            })
-            ->callbackAfterInsert(function ($stateParameters) {
-                $existingPdfPath = FCPATH . 'assets/uploads/cts/' . $this->cts->find($stateParameters->insertId)['adjunto'];
-
-                // Create new FPDI instance
-                $pdf = new Fpdi();
-
-                // Disable automatic page break
-                $pdf->SetAutoPageBreak(false, 0);
-
-                $pdf->setPrintHeader(false);
-
-                // Import the existing PDF
-                $pdf->setSourceFile($existingPdfPath);
-
-                // We'll only work with the first page
-                $templateId = $pdf->importPage(1);
-                $size = $pdf->getTemplateSize($templateId);
-
-                // Add only one page
-                $pdf->AddPage($size['orientation'], array($size['width'], $size['height']));
-
-                // Use the template
-                $pdf->useTemplate($templateId, 0, 0, $size['width'], $size['height'], true);
-
-                $firmaPath = FCPATH . 'assets/uploads/firmas/' . $this->usuarios->find(5)['firma'];
-
-                if (is_file($firmaPath) && is_readable($firmaPath)) {
-                    // Get signature dimensions
-                    list($sigWidth, $sigHeight) = getimagesize($firmaPath);
-
-                    // Calculate scaling factor to fit within 20x10
-                    $scale = min(70 / $sigWidth, 30 / $sigHeight);
-                    $newWidth = $sigWidth * $scale;
-                    $newHeight = $sigHeight * $scale;
-
-                    $x = 40;  // Adjust this value to move left or right
-    
-                    // Second signature position (adjust as needed)
-                    $y = 218; // Adjust this value to move up or down
-    
-                    // Add the second signature image
-                    $pdf->Image($firmaPath, $x, $y, $newWidth, $newHeight, 'PNG');
-                } else {
-                    log_message('error', 'Signature file not found or not readable: ' . $firmaPath);
-                }
-
-                // Save the modified PDF
-                $pdf->Output($existingPdfPath, 'F');
-                return $stateParameters;
-            })
-            ->displayAs([
-                'id_usuario' => 'EMPLEADO',
-                'fecha_creacion' => 'FECHA DE CREACION',
-                'adjunto' => 'BOLETA',
-                'id_estado_boleta' => 'ESTADO DE LA BOLETA',
-                'subido_por' => 'SUBIDO POR',
-                'fecha_modificacion' => 'FECHA DE MODIFICACION',
-                'revisado_por' => 'REVISADO POR',
-                'observaciones' => 'OBSERVACIONES',
-                'boleta_firmada' => 'FIRMADA',
-                'visto' => 'VISTO'
             ]);
+        // Check if current operation is export  
+        if ($this->gc->getState() === 'Export') {
+            $this->gc->callbackColumn('boleta_firmada', function ($value, $row) {
+                return ($value == '0') ? 'SI' : 'NO';
+            });
+            $this->gc->callbackColumn('visto', function ($value, $row) {
+                return ($value == '0') ? 'NO' : 'SI';
+            });
+        }
+        $this->gc->callbackBeforeInsert(function ($stateParameters) {
+            $filename = $stateParameters->data['adjunto'];
+            $fileExtension = pathinfo($filename, PATHINFO_EXTENSION);
+            if (strtolower($fileExtension) !== 'pdf') {
+                $errorMessage = new \GroceryCrud\Core\Error\ErrorMessage();
+                return $errorMessage->setMessage("Solo puede subir PDFs!\n");
+            }
+            $stateParameters->data['subido_por'] = session()->get('user_id');
+            $stateParameters->data['id_estado_boleta'] = 2;
+            $stateParameters->data['revisado_por'] = 5;
+            $stateParameters->data['boleta_firmada'] = 1; // Not signed yet
+            $stateParameters->data['visto'] = 0; // Not viewed yet
+            return $stateParameters;
+        })
+        ->callbackAfterInsert(function ($stateParameters) {
+            $existingPdfPath = FCPATH . 'assets/uploads/cts/' . $this->cts->find($stateParameters->insertId)['adjunto'];
+
+            // Create new FPDI instance
+            $pdf = new Fpdi();
+
+            // Disable automatic page break
+            $pdf->SetAutoPageBreak(false, 0);
+
+            $pdf->setPrintHeader(false);
+
+            // Import the existing PDF
+            $pdf->setSourceFile($existingPdfPath);
+
+            // We'll only work with the first page
+            $templateId = $pdf->importPage(1);
+            $size = $pdf->getTemplateSize($templateId);
+
+            // Add only one page
+            $pdf->AddPage($size['orientation'], array($size['width'], $size['height']));
+
+            // Use the template
+            $pdf->useTemplate($templateId, 0, 0, $size['width'], $size['height'], true);
+
+            $firmaPath = FCPATH . 'assets/uploads/firmas/' . $this->usuarios->find(5)['firma'];
+
+            if (is_file($firmaPath) && is_readable($firmaPath)) {
+                // Get signature dimensions
+                list($sigWidth, $sigHeight) = getimagesize($firmaPath);
+
+                // Calculate scaling factor to fit within 20x10
+                $scale = min(70 / $sigWidth, 30 / $sigHeight);
+                $newWidth = $sigWidth * $scale;
+                $newHeight = $sigHeight * $scale;
+
+                $x = 40;  // Adjust this value to move left or right
+
+                // Second signature position (adjust as needed)
+                $y = 218; // Adjust this value to move up or down
+
+                // Add the second signature image
+                $pdf->Image($firmaPath, $x, $y, $newWidth, $newHeight, 'PNG');
+            } else {
+                log_message('error', 'Signature file not found or not readable: ' . $firmaPath);
+            }
+
+            // Save the modified PDF
+            $pdf->Output($existingPdfPath, 'F');
+            return $stateParameters;
+        })
+        ->displayAs([
+            'id_usuario' => 'EMPLEADO',
+            'fecha_creacion' => 'FECHA DE CREACION',
+            'adjunto' => 'BOLETA',
+            'id_estado_boleta' => 'ESTADO DE LA BOLETA',
+            'subido_por' => 'SUBIDO POR',
+            'fecha_modificacion' => 'FECHA DE MODIFICACION',
+            'revisado_por' => 'REVISADO POR',
+            'observaciones' => 'OBSERVACIONES',
+            'boleta_firmada' => 'FIRMADA',
+            'visto' => 'VISTO'
+        ]);
 
         // Rendering the CRUD
         $output = $this->gc->render();
