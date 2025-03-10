a908d4a00c308f50ffcadfc1d6bbc3546d700268
Omar Vásquez <omarvs91@gmail.com>
2025-03-10 00:32:27 -0500

    Creando la tabla 'configuracion' para almacenar configuraciones del sistema.

---
+++ app/Database/Migrations/2025-10-09-225100_create_configuracion_table.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateConfiguracionTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'id_usuario_doc_firma' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('configuracion');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('configuracion');
+    }
+}
